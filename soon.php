86e8e5515f45eacb808a91ffa21ee788c9cdc4e5
unassailable <github@unassailable.info>
2015-06-22 16:54:50 -0400

    landing page set

---
+++ soon.php
@@ -0,0 +1,9 @@
+				<div class="tileset">
+					<div class="tile soon" style="background-image:url(/media/tile_border.png),url(/media/soon.png);">
+					</div>
+					<div class="soon-text">
+						The new site is being worked on and should be launching in a few weeks.
+						<br/><br/>
+						Feel free to follow the progress on the <a href="https://github.com/unassailable/uinfo">GitHub page</a> until then.
+					</div>
+				</div>
